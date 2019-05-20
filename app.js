dcee0e6183864b72882389dacf3904f858213915
JanakPradhan <memories.janak@gmail.com>
2019-05-20 14:36:52 +0545

    final

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $(document).on("click", "h2", function() {
+$(document).ready(function () {
+  $(document).on("click", ".faq h2", function () {
     $(this)
       .siblings("p")
       .stop()
@@ -12,18 +12,18 @@ $(document).ready(function() {
       .slideUp();
   });
 
-  $.each($(".faq__items h2"), function(curr, elem) {
-    elem.addEventListener("click", function() {
+  $.each($(".faq__items h2"), function (curr, elem) {
+    elem.addEventListener("click", function () {
       elem.classList.toggle("is-opened");
     });
   });
 
-  $(".header__sm").click(function() {
+  $(".header__sm").click(function () {
     $(".header__sm").toggleClass("active");
     $("#js-nav").toggleClass("is-opened");
   });
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 200) {
       $(".scroll__to__top").fadeIn();
     } else {
@@ -31,13 +31,37 @@ $(document).ready(function() {
     }
   });
 
-  $(".scroll__to__top").click(function() {
-    $("html, body").animate(
-      {
+  $(".scroll__to__top").click(function () {
+    $("html, body").animate({
         scrollTop: 0
       },
       1200
     );
     return false;
+  });
+});
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
 });
